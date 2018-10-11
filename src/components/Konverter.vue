437bb9384dedddbbc8117b001a9e5b2f28123bae
Riina Häidkind <riina.haidkind@khk.ee>
2018-10-11 12:50:52 +0300

    Lisatud teised arvutustehted, tehte loogika liigutatud meetodisse, lisatud konditsionaalne renderdamine.

--- src/components/Konverter.vue
+++ src/components/Konverter.vue
@@ -3,18 +3,18 @@
       <h1>Kaalu teisendamine</h1>
       <h4>Sisesta kaal kilogrammides:</h4>
       <input type="number" min="0" v-model="kg">
-      <table>
+      <table v-if="kg !== 0">
           <tr>
               <td>Grammid</td>
-              <td>{{`${kg * 1000} g` }}</td>
+              <td>{{convertKg(1000) + 'g' }}</td>
           </tr>
           <tr>
               <td>Naelad</td>
-              <td></td>
+              <td> {{convertKg(2.20462262) + 'lb' }}</td>
           </tr>
           <tr>
               <td>Untsid</td>
-              <td></td>
+              <td>{{convertKg(35.2739619) + 'oz' }}</td>
           </tr>
       </table>
   </div>
@@ -28,8 +28,15 @@ export default {
           kg: 0
       }
   },
-    methods: {
-        
+  methods: {
+    convertKg(multiplier){
+            if(this.kg !== 0) {
+            return (this.kg * multiplier).toFixed(2)
+            } else {
+                return 0 
+            }
+        return (this.kg !== 0) ? (this.kg * multiplier).toFixed(2) : 0
+        }
     }
 }
 </script>
